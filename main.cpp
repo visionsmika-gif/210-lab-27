8bdca068d6fbb13c8e75dd7b9bcb35563bbf5bd4
visionsmika-gif <visionsmika@gmail.com>
2025-10-29 17:08:48 -0700

    update

--- main.cpp
+++ main.cpp
@@ -4,10 +4,50 @@
 #include <tuple>
 using namespace std;
 
+// Function to display a menu of choices labeled 1-4 to the user.
+// Args:    none
+// Returns: the the user's choice (an int).
+int getUserChoice() {
+    int choice;
+    cout << "1. Increase Friendship\n";
+    cout << "2. Decrease Friendship\n";
+    cout << "3. Search for Villager\n";
+    cout << "4. Exit\n";
+    do {
+        cout << "Enter choice --> ";
+        cin >> choice;
+        if (choice < 1 || choice > 4) {
+            cout << "Invalid choice. Please enter 1, 2, 3, or 4.\n";
+        }
+    } while (choice < 1 || choice > 4);
+    return choice;
+}
+
 int main() {
     // The map's value has been modified to a tuple with a friendship level (int), species (string), and catchphrase (string).
     map<string, tuple<int, string, string>> vilagerInfo;
 
+    int choice = getUserChoice();
+
+    while (choice != 4) {
+        if (choice == 1) {
+            increaseFriendship(villagerInfo);
+        }
+        else if (choice == 2) {
+            decreaseFriendship(villagerInfo);
+        }
+        else if (choice == 3) {
+            searchForVillager(villagerInfo);
+        }
+        else {
+            //
+        }
+        choice = getUserChoice();
+    }
+
+
+
+    /*
     // insert elements into the map
     // note how the right-hand side of the assignment are the vector elements
     vilagerInfo["Audie"] = {0, "Wolf", "Foxtrot"};
@@ -53,6 +93,7 @@ int main() {
     cout << "\nSize before clear: " << vilagerInfo.size() << endl;
     vilagerInfo.clear();
     cout << "Size after clear: " << vilagerInfo.size() << endl;
+    */
 
     return 0;
 }
